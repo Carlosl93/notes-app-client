d27d2c8c5aab38180153c6acae79b4253085188c
Carlosl93 <carlosluis.43@gmail.com>
2020-04-12 19:57:28 -0400

    Added Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "chernandez-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://6wop60tx9i.execute-api.us-east-2.amazonaws.com/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_M3DHffQ33D",
+    APP_CLIENT_ID: "4h8v9tiuv4bg1p5slk0nruti4s",
+    IDENTITY_POOL_ID: "us-east-2:70da95e1-93a1-4f9e-afa4-75b3f79b4aef"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
+import { Amplify } from "aws-amplify";
+import config from "./config";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -13,6 +15,30 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
